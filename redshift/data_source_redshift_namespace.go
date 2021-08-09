97e0ab561a296a3eaa708bf503753bc93987ec85
Steve Swor <sworisbreathing@users.noreply.github.com>
2021-08-09 10:12:38 +1000

    Add `redshift_namespace` data source

---
+++ redshift/data_source_redshift_namespace.go
@@ -0,0 +1,22 @@
+package redshift
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceRedshiftNamespace() *schema.Resource {
+	return &schema.Resource{
+		Description: `Gets the cluster namespace (unique ID) of the Amazon Redshift cluster.`,
+		Read:        RedshiftResourceFunc(dataSourceRedshiftNamespaceRead),
+		Schema:      map[string]*schema.Schema{},
+	}
+}
+
+func dataSourceRedshiftNamespaceRead(db *DBConnection, d *schema.ResourceData) error {
+	var namespace string
+	if err := db.QueryRow("SELECT CURRENT_NAMESPACE").Scan(&namespace); err != nil {
+		return err
+	}
+	d.SetId(namespace)
+	return nil
+}

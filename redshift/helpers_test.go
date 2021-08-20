bcc236c892645427b5addba2b8232fa61792c7e2
Wojciech Inglot <wojciech@inglot.net.pl>
2021-08-20 13:39:53 +0200

    Implement redshift_default_privileges resource

---
+++ redshift/helpers_test.go
@@ -0,0 +1,64 @@
+package redshift
+
+import (
+	"testing"
+)
+
+func TestValidatePrivileges(t *testing.T) {
+	tests := map[string]struct {
+		privileges []string
+		objectType string
+		expected   bool
+	}{
+		"valid list for schema": {
+			privileges: []string{"create", "usage"},
+			objectType: "schema",
+			expected:   true,
+		},
+		"invalid list for schema": {
+			privileges: []string{"foo"},
+			objectType: "schema",
+			expected:   false,
+		},
+		"extended invalid list for schema": {
+			privileges: []string{"create", "usage", "insert"},
+			objectType: "schema",
+			expected:   false,
+		},
+		"empty list for schema": {
+			privileges: []string{},
+			objectType: "schema",
+			expected:   true,
+		},
+		"valid list for table": {
+			privileges: []string{"insert", "update", "delete", "select", "references"},
+			objectType: "table",
+			expected:   true,
+		},
+		"invalid list for table": {
+			privileges: []string{"foobar"},
+			objectType: "schema",
+			expected:   false,
+		},
+		"extended invalid list for table": {
+			privileges: []string{"create", "usage", "insert"},
+			objectType: "table",
+			expected:   false,
+		},
+		"empty list for table": {
+			privileges: []string{},
+			objectType: "table",
+			expected:   true,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := validatePrivileges(tt.privileges, tt.objectType)
+
+			if result != tt.expected {
+				t.Errorf("Expected result to be `%t` but got `%t`", tt.expected, result)
+			}
+		})
+	}
+}
--- redshift/resource_redshift_privilege_test.go
+++ redshift/resource_redshift_privilege_test.go
@@ -7,65 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestValidatePrivileges(t *testing.T) {
-	tests := map[string]struct {
-		privileges []string
-		objectType string
-		expected   bool
-	}{
-		"valid list for schema": {
-			privileges: []string{"create", "usage"},
-			objectType: "schema",
-			expected:   true,
-		},
-		"invalid list for schema": {
-			privileges: []string{"foo"},
-			objectType: "schema",
-			expected:   false,
-		},
-		"extended invalid list for schema": {
-			privileges: []string{"create", "usage", "insert"},
-			objectType: "schema",
-			expected:   false,
-		},
-		"empty list for schema": {
-			privileges: []string{},
-			objectType: "schema",
-			expected:   true,
-		},
-		"valid list for table": {
-			privileges: []string{"insert", "update", "delete", "select", "references"},
-			objectType: "table",
-			expected:   true,
-		},
-		"invalid list for table": {
-			privileges: []string{"foobar"},
-			objectType: "schema",
-			expected:   false,
-		},
-		"extended invalid list for table": {
-			privileges: []string{"create", "usage", "insert"},
-			objectType: "table",
-			expected:   false,
-		},
-		"empty list for table": {
-			privileges: []string{},
-			objectType: "table",
-			expected:   true,
-		},
-	}
-
-	for name, tt := range tests {
-		t.Run(name, func(t *testing.T) {
-			result := validatePrivileges(tt.privileges, tt.objectType)
-
-			if result != tt.expected {
-				t.Errorf("Expected result to be `%t` but got `%t`", tt.expected, result)
-			}
-		})
-	}
-}
-
 func TestAccRedshiftPrivilege_Basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },

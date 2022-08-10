ac840df67d67b9ee940182b06d926eb0a66e46dc
Michael Tesch <mtesch@utilimarc.com>
2022-08-10 14:20:48 -0500

    example cross account resource name

--- examples/resources/redshift_datashare_privilege/resource.tf
+++ examples/resources/redshift_datashare_privilege/resource.tf
@@ -11,7 +11,7 @@ resource "redshift_datashare_privilege" "within_account" {
 # Example: cross-account datashare permission.
 # Note: you will also need to authorize the cross-account datashare
 # in the AWS console after creating this resource
-resource "redshift_datashare_privilege" "within_account" {
+resource "redshift_datashare_privilege" "cross_account" {
   name = redshift_datashare.share.name # Required
   account = "123456789012" # Required
 }

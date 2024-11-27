c6ed26acf2e22d48d136615fb164413ed8c4d2f9
Josep Pla Beltrán <josep.pla@jobandtalent.com>
2024-11-27 13:41:05 +0100

    Removing debug

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
@@ -12,14 +10,7 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
-    var debug bool
-
-    flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
-    flag.Parse()
-
 	plugin.Serve(&plugin.ServeOpts{
-		Debug : debug,
-		ProviderAddr: "jt.dev/tf/redshifttf",
 		ProviderFunc: func() *schema.Provider {
 			return redshift.Provider()
 		},
